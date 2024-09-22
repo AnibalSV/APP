3cb861a4619b342585f1e43e46d773e3a006cf23
andres <jorquera.ad@gmail.com>
2024-09-22 18:12:52 +0300

    feat: new form component

---
+++ src/pages/NewLogin/index.tsx
@@ -0,0 +1,1 @@
+export { default, type Props as NewLoginProps } from './Login';
--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Login } from "./pages/Login/Login";
+
+import NewLogin from "./pages/NewLogin";
 import OuterLayout from "./layouts/Outer.layout";
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import Register from "./pages/Register/Register";
 import ForgotPassConfirm from "./pages/ForgotPass/ForgotPassConfirm";
 import DashboardLayout from "./layouts/Dashboard.layout";
+import { Login } from "./pages/Login/Login";
+
 
 const router = createBrowserRouter([
     {
@@ -14,6 +17,10 @@ const router = createBrowserRouter([
         {
           path: "login",
           element: <Login />,
+        },
+        {
+          path: "new-login",
+          element: <NewLogin />,
         },
         {
           path: "register",
