c65a1c0bac469d5ad4edcab4c44d2f54eb4448a2
Anibal Sequera <anibalsequera2024@gmail.com>
2024-09-23 19:55:49 +0200

    preview configuration

--- src/components/Form.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Login } from './Login/Login';
-
-export const Form = () => {
-    const mensaje = 'Esto va desde el componente padre';
-    return (
-            <Login msj={mensaje} />
-    )
-}
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { configDefaults } from 'vitest/config';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/App/',
   plugins: [react()],
-})
+  test: {
+    coverage: {
+      reporter: ['text', 'lcov'], 
+      reportsDirectory: './coverage',
+    },
+  },
+});
