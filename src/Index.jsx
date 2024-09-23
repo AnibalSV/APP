908047652b2455bc7172365f4ad27cc8f745b54b
Anibal Sequera <anibalsequera2024@gmail.com>
2024-09-23 20:23:56 +0200

    delete file

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,8 +1,6 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import {Incrementar} from '../src/components/counter';
-import { Value } from './components/Text';
 import "./Index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
